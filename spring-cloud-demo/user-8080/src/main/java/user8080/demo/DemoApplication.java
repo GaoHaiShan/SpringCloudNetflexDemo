cb7d3706570ff72b78cded1d384c2351769cdf3f
GaoHaiShan <1014323626@qq.com>
2020-09-16 11:50:25 +0800

    cloud and fegin

--- spring-cloud-demo/user-8080/src/main/java/user8080/demo/DemoApplication.java
+++ spring-cloud-demo/user-8080/src/main/java/user8080/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package user8080.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients(basePackages = {"org.order.api"})
 public class DemoApplication {
 
     public static void main(String[] args) {
--- spring-cloud-demo/user-8080/src/main/java/user8080/demo/User.java
+++ spring-cloud-demo/user-8080/src/main/java/user8080/demo/User.java
@@ -1,12 +1,24 @@
 package user8080.demo;
 
+import org.order.api.IOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class User {
-    @GetMapping("/user")
-    public String userInfo(){
-        return "user";
+
+    @Autowired
+    private IOrder order;
+
+    @Autowired
+    private IOrder.OrderFallback fallback;
+
+    @GetMapping("/orders")
+    public String orders(){
+
+        return fallback.getOrders() + order.getOrders();
     }
+
+
 }
